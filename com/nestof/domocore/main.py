7c238279096de3eef0b7164dfd076009e83d9ae4
nestof <nestof@Gandalf>
2014-04-10 22:51:35 +0200

    MAJ pour utilisation des enumerations

--- com/nestof/domocore/main.py
+++ com/nestof/domocore/main.py
@@ -8,11 +8,16 @@ Created on 21 mars 2014
 
 from com.nestof.domocore import enumeration
 from com.nestof.domocore import utils
+from com.nestof.domocore.service.MCZProtocolService import MCZProtocolService
 
 
 if __name__ == '__main__':
     print(utils.getFlag(enumeration.Ordre.off, enumeration.NiveauPuissance.niveau1 ,enumeration.NiveauVentilation.auto,  enumeration.Actionneur.systeme))
-    None
+    
+    mczProtocolService = MCZProtocolService(None)
+    trame = mczProtocolService.getTrame(enumeration.NiveauPuissance.niveau3, enumeration.NiveauVentilation.niveau3, enumeration.Mode.automatique, enumeration.Etat.on, enumeration.Actionneur.utilisateur)
+    print(trame)
+    print(utils.binaryStringToHex(trame))
     
 
         
