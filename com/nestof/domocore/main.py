caea0efbbceab908a01d55d26e88d7a2747d8caa
nestof <nestof@gmail.com>
2014-04-15 14:21:46 +0200

    Starting algo

--- com/nestof/domocore/main.py
+++ com/nestof/domocore/main.py
@@ -23,11 +23,56 @@ if __name__ == '__main__':
     
     
     currentMode = databaseService.findCurrentMode()
-    if currentMode  != None : 
+    
+    
+    onPeriode = currentMode  != None
+    offPeriode = currentMode  == None
+    
+    if onPeriode : 
         print("Mode : " + currentMode._libelle)
         print("Consigne : " + str(currentMode._cons))
         print("Max : " + str(currentMode._max))
 
+
+    poeleActive = databaseService.getPoeleActive() 
+    forcedOn = databaseService.getForcedOn()
+    forcedOff = databaseService.getForcedOff()
+
+    print("Poêle actif : " + str(poeleActive))
+    print("Marche forcée : " + str(forcedOn))
+    print("Arrêt forcé : " + str(forcedOff))
+    
+    
+    
+    
+    """RAZ forced stat"""
+    if offPeriode and forcedOff :
+        #TODO RAZ forcedOff
+        None
+    elif onPeriode and forcedOn :
+        #TODO RAZ forcedOn
+        None
+        
+    """ Init start and stop """
+    start = False
+    stop = False
+    if onPeriode or forcedOn :
+        start = True
+        stop = False
+    elif offPeriode or forcedOff :
+        start = False
+        stop = True
+    
+    if onPeriode :
+        #TODO Read current temp
+        currentTemp = 17.0
+        if currentTemp >= currentMode._cons :
+            stop = True
+            start = False
+        else :
+            #TODO déterminer les niveaux
+            None
+    
        
     trame = mczProtocolService.getTrame(enumeration.NiveauPuissance.niveau3, enumeration.NiveauVentilation.niveau3, enumeration.Mode.automatique, enumeration.Etat.on, enumeration.Actionneur.utilisateur)
     print("Trame message : " + utils.binaryStringToHex(trame._message))
