d67a013b5156695f90552c27e6074624a717e501
nestof <nestof@gmail.com>
2014-09-03 15:49:28 +0200

    Ajout des scripts de creation de la base de données + alimentation de la table parametrage

---
+++ sql/TABLE PARAMETRAGE.sql
@@ -0,0 +1,6 @@
+INSERT INTO "parametrage" VALUES('POELE_MARCHE_FORCEE','BOOLEAN','FALSE','Indique si le poêle doit être mis en marche forcée. Valeurs possibles : TRUE, FALSE');
+INSERT INTO "parametrage" VALUES('POELE_ARRET_FORCE','BOOLEAN','FALSE','Indique si le poêle doit être mis en arrêt forcé. Valeurs possibles : TRUE, FALSE');
+INSERT INTO "parametrage" VALUES('POELE_ETAT','STRING','OFF','Indique l''état du poêle. Valeurs possibles : ON,OFF');
+INSERT INTO "parametrage" VALUES('TEMP_MAXI_MARCHE_FORCEE','FLOAT','22','Température maxi à utiliser en marche forcée si aucun mode n''est défini');
+INSERT INTO "parametrage" VALUES('TEMP_CONSIGNE_MARCHE_FORCEE','FLOAT','19','Température de consigne à utiliser en marche forcée si aucun mode n''est défini');
+INSERT INTO "parametrage" VALUES('DERNIER_MODE','INTEGER','1','Dernier mode utilisé');
